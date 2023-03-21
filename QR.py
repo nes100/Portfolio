76537dceefe880300a65e447f674930122df5f94
Anes <suhonjicanes96@gmail.com>
2023-03-20 19:50:33 -0500

    Initial commit

---
+++ QR.py
@@ -0,0 +1,14 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=4,
+)
+
+data = "https://nes100-my-porfolio-app-home-czwd4x.streamlit.app/"
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("Website-QRcode.png")
---
+++ pages/pages/Certificates.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.image("certs/sec+.png")
